60de8e4329faef26551ba74e702fb5b4ed861220
YalenniA <yalenni109arias@gmail.com>
2025-02-26 14:26:22 -0600

    Add files via upload

---
+++ challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,40 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById('amigo')
+    let nombre = input.value.trim();
+
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+
+}
+
+function actualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    amigos.forEach(amigo => {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No hay amigos para sortear.');
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    document.getElementById('resultado').innerHTML =  `🎉 El amigo sorteado es: <strong>${amigoSorteado}</strong> 🎉`;
+}
